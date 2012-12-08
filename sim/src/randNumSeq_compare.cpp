cb6bc8875508ffb89bbc51faa4978d7b995e5d2c
sshahulh <sshahulh@hawk.iit.edu>
2012-12-08 15:59:22 +0000

    Comparing the sequence and showing the result

--- sim/src/randNumSeq_compare.cpp
+++ sim/src/randNumSeq_compare.cpp
@@ -3,19 +3,20 @@
 #include<time.h>
 #include<math.h>
 #include<string.h>
+#include <iostream>
+#include <vector>
 #include "simLibAdv.h"
 
+int compare4qsort (const void * a, const void * b);
+void findCommon (double A[], double B[], int iter);
 
-
-int compare (const void * a, const void * b)
-{
-  //	return ( *(int*)a - *(int*)b );
-	const double *da = (const double *) a;
-	const double *db = (const double *) b;
-	return (*da > *db) - (*da < *db);
+void Usage(){
+   printf("\nUsage : randNumSeq_compare  [maxitems_in_seq] \n");
+   printf("maxitems_in_seq (Default value 10) : This parameter denotes how many random number needs to be generated per sequence \n");
 }
 
 int main(int argc,char *argv[]){
+    Usage();
     int num_seq=2;
     double r_d;
     unsigned int seed=0;
@@ -23,12 +24,15 @@ int main(int argc,char *argv[]){
     clock_t start;
     struct tm *loc_t;
     int num_divisions=1000;
-    int num_iterations=10;
+    int maxitems_in_seq=10;
+    if (argc > 1){
+        maxitems_in_seq = atoi(argv[1]);
+    }
     double r_unit ;
     RandomGen rng(seed);
     
-    double rnd_seq1[num_iterations];
-    double rnd_seq2[num_iterations]; //TODO
+    double rnd_seq1[maxitems_in_seq];
+    double rnd_seq2[maxitems_in_seq]; //TODO  can be convered to a 2D array instead of two 1D arrays ?
 
     
     for (int j=0; j < num_seq; j++ ) {
@@ -46,7 +50,7 @@ int main(int argc,char *argv[]){
     	printf("Sample Rand Number = %lf \n",r_d);
    
 	// Generate and store random numbers	
-	for(int i=0;i<num_iterations;i++) {
+	for(int i=0;i<maxitems_in_seq;i++) {
 		// UGLY FIX ME
 		if (j==0) {
         		rnd_seq1[i] = rng.rand_d();
@@ -65,31 +69,81 @@ int main(int argc,char *argv[]){
 
 
       	printf("\nSequence 1 \n");
-    	for(int i=0;i<num_iterations;i++){
+    	for(int i=0;i<maxitems_in_seq;i++){
         	printf("%f \n",rnd_seq1[i]);
         	//printf("%1.3f \n",rnd_seq1[i]);
     	}
 
       	printf("Sequence 2 \n");
-        for(int i=0;i<num_iterations;i++){
+        for(int i=0;i<maxitems_in_seq;i++){
                 printf("%f \n",rnd_seq2[i]);
         }
 
-	qsort (rnd_seq1, num_iterations, sizeof(double), compare);
-	qsort (rnd_seq2, num_iterations, sizeof(double), compare);
+	qsort (rnd_seq1, maxitems_in_seq, sizeof(double), compare4qsort);
+	qsort (rnd_seq2, maxitems_in_seq, sizeof(double), compare4qsort);
 
 	printf("\nSequence 1 (Sorted)\n");
-        for(int i=0;i<num_iterations;i++){
+        for(int i=0;i<maxitems_in_seq;i++){
                 printf("%f \n",rnd_seq1[i]);
                 //printf("%1.3f \n",rnd_seq1[i]);
         }
 
         printf("Sequence 2 (Sorted)\n");
-        for(int i=0;i<num_iterations;i++){
+        for(int i=0;i<maxitems_in_seq;i++){
                 printf("%f \n",rnd_seq2[i]);
         }
+
+	//http://www.leetcode.com/2010/03/here-is-phone-screening-question-from.html
+
+	findCommon(rnd_seq1,rnd_seq2,maxitems_in_seq);
 
 
 
     return 0;
+}
+
+
+
+int compare4qsort (const void * a, const void * b)
+{
+  //    return ( *(int*)a - *(int*)b );
+        const double *da = (const double *) a;
+        const double *db = (const double *) b;
+        return (*da > *db) - (*da < *db);
+}
+
+
+void  findCommon(double A[], double B[], int iter) {
+  	int i = 0, j = 0;
+        int count =0;
+  	while (i < iter && j < iter) {
+    		if (A[i] > B[j]) {
+        		printf ( "%f > %f",A[i],B[j]);
+			printf("\ta is greater\n");
+      			j++;
+    		} else if (B[j] > A[i]) {
+        		printf ( "%f < %f",A[i],B[j]);
+			printf("\tb is greater\n");
+      			i++;
+    		} else {
+			printf("\t Found common %d\n", A[i]);
+      			i++;
+      			j++;
+			count++;
+    		}
+  	}
+
+	if (count == 0) {
+
+		 printf("\t ##########################################################################################\n");
+		 printf("\t There are no common items found  between the two generated sequence each of length %d\n",count,iter);
+		 printf("\t ##########################################################################################\n");
+	} else {
+
+		 printf("\t ######################################################################################\n");
+                 printf("\t There are %d common items found between the two generated sequence each of length %d\n",count,iter);
+                 printf("\t ######################################################################################\n");
+		
+	}
+	
 }
