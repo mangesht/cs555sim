14b692351d03b68a680be25051ecb07cdc61d80e
mangesh.thakare <mangesh.thakare@gmail.com>
2012-12-09 19:49:13 +0000

    Initial version

--- sim/scripts/run.sh
+++ sim/scripts/run.sh
@@ -20,17 +20,23 @@ mv simruni7_w15avg.dat simruni7_w15avgA.dat
 gnuplot ../scripts/simruni7_w15A.p
 
 # System B 
-./sim -lambda 10.0 -u 1.0 -ic 0 -et 10 -w 500 -itr 1000 -sci 0.001 -mode 0 -s1 12345 -s2 34567 -s3 78901 >a
+./sim -lambda 10.0 -u 1.0 -ic 0 -et 20 -w 500 -itr 10000 -sci 0.001 -mode 0 -s1 12345 -s2 34567 -s3 78901 >a
 mv simruni0_w500avg.dat simruni0_w500avgB.dat 
 ../scripts/plot_avg.sh simruni0_w500avgB.dat > ../scripts/simruni0_w500B.p
 gnuplot ../scripts/simruni0_w500B.p
 
-./sim -lambda 10.0 -u 1.0 -ic 4 -et 10 -w 500 -itr 1000 -sci 0.001  -mode 0  -s1 526589 -s2 1032598 -s3 7854689 >a
+./sim -lambda 10.0 -u 1.0 -ic 0 -et 20 -w 50 -itr 10000 -sci 0.001 -mode 0 -s1 526589 -s2 1032598 -s3 7854689 >a
+mv simruni0_w50avg.dat simruni0_w50avgBSeed.dat 
+../scripts/plot_avg.sh simruni0_w50avgBSeed.dat > ../scripts/simruni0_w50BSeed.p
+gnuplot ../scripts/simruni0_w50BSeed.p
+
+
+./sim -lambda 10.0 -u 1.0 -ic 4 -et 20 -w 500 -itr 10000 -sci 0.001  -mode 0  -s1 526589 -s2 1032598 -s3 7854689 >a
 mv simruni4_w500avg.dat simruni4_w500avgB.dat
 ../scripts/plot_avg.sh simruni4_w500avgB.dat > ../scripts/simruni4_w500B.p
 gnuplot ../scripts/simruni4_w500B.p
 
-./sim -lambda 10.0 -u 1.0 -ic 4 -et 10 -w 15 -itr 1000 -sci 0.001  -mode 0  -s1 895626 -s2 2659842 -s3 2365598 >a
+./sim -lambda 10.0 -u 1.0 -ic 4 -et 20 -w 15 -itr 10000 -sci 0.001  -mode 0  -s1 895626 -s2 2659842 -s3 2365598 >a
 mv simruni4_w15avg.dat simruni4_w15avgB.dat
 ../scripts/plot_avg.sh simruni4_w15avgB.dat > ../scripts/simruni4_w15B.p
 gnuplot ../scripts/simruni4_w15B.p
